e3048c63401c97e5e2d70d469896c5b002ba7efa
pruthvipatnala <pruthvipatnala@gmail.com>
2019-02-07 20:52:43 +0530

    status codes notes added

---
+++ Assignment-2/flask.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  7 18:38:13 2019
+
+@author: pruthvi
+"""
+
+from flask import Flask,render_template ,redirect, url_for , request
+import requests
+import sqlite3 as sql
+
+
+
