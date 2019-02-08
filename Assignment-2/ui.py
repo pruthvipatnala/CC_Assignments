258e8248310a1fd49defa843e9e58a0e5b6c2699
pruthvipatnala <pruthvipatnala@gmail.com>
2019-02-08 10:37:10 +0530

    minor changes added

--- Assignment-2/ui.py
+++ Assignment-2/ui.py
@@ -1,7 +1,7 @@
-from flask import Flask,render_template ,redirect, url_for , request
+from flask import Flask,render_template ,redirect, url_for , request, jsonify
 import requests
 import sqlite3 as sql
-import jsonify
+# /import jsonify
 
 
 app = Flask(__name__)
@@ -16,14 +16,34 @@ def add_user(details):
     conn.execute(command)
     conn.commit()
 
-'''
-@app.route("/api/v1/users",methods=["POST","GET","DELETE","PUT"])
+
+@app.route('/api/v1/users',methods=["POST","GET","DELETE","PUT"])
 def api_add_user():
     if request.method == 'POST':
+        conn=sql.connect("assign.db")
+        command = "select count(*) from user;"
+        n = int(list(conn.execute(command))[0][0])
+        conn.commit()
+        #print(n)
+        userDataInJsonFormat = (request.get_json())
+        user_id = userDataInJsonFormat['user_id']
+        user_name = userDataInJsonFormat['username']
+        contact_no = userDataInJsonFormat['contact_no']
+        email_id = userDataInJsonFormat['email_id']
+        password = userDataInJsonFormat['password']
         
-'''
+        details = [user_id,user_name,contact_no,email_id,password]
+        print(details)
+        add_user(details)
+        
+        return jsonify({}),200
+        
+        
+    return render_template('test.html')
+        
+
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port = 5001)
