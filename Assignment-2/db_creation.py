f4840afd9f19a5accdca15b08274ec3aef329a59
pruthvipatnala <pruthvipatnala@gmail.com>
2019-02-08 10:43:54 +0530

    db_schema changed

--- Assignment-2/db_creation.py
+++ Assignment-2/db_creation.py
@@ -11,10 +11,7 @@ import sqlite3 as sql
 conn=sql.connect("assign.db")
 
 conn.execute("CREATE TABLE user (\
-             user_id TEXT NOT NULL,\
-             user_name TEXT,\
-             contact_no TEXT UNIQUE NOT NULL,\
-             email_id TEXT,\
+             user_name TEXT NOT NULL,\
              password TEXT NOT NULL,\
-             PRIMARY KEY(user_id));")
+             PRIMARY KEY(user_name));")
 
