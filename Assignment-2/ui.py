082881c272c1a93074b9b630f2062cd49946ef26
pruthvipatnala <pruthvipatnala@gmail.com>
2019-02-08 20:22:28 +0530

    two apis working

--- Assignment-2/ui.py
+++ Assignment-2/ui.py
@@ -1,7 +1,8 @@
 from flask import Flask,render_template ,redirect, url_for , request, jsonify
 import requests
 import sqlite3 as sql
-# /import jsonify
+import hashlib
+#import jsonify
 
 
 app = Flask(__name__)
@@ -12,7 +13,13 @@ def home():
 def add_user(details):
     #details = [user_id,user_name,contact_no,email_id,password]
     conn=sql.connect("assign.db")
-    command = "INSERT INTO user(user_id,user_name,contact_no,email_id,password) values ('"+str(details[0])+"','"+str(details[1])+"','"+str(details[2])+"','"+str(details[3])+"','"+str(details[4])+"');"
+    command = "INSERT INTO user(user_name,password) values ('"+str(details[0])+"','"+str(details[1])+"');"
+    conn.execute(command)
+    conn.commit()
+
+def remove_user(user_name):
+    conn=sql.connect("assign.db")
+    command = "DELETE FROM user WHERE user_name='"+str(user_name)+"';"
     conn.execute(command)
     conn.commit()
 
@@ -20,19 +27,17 @@ def add_user(details):
 @app.route('/api/v1/users',methods=["POST","GET","DELETE","PUT"])
 def api_add_user():
     if request.method == 'POST':
-        conn=sql.connect("assign.db")
-        command = "select count(*) from user;"
-        n = int(list(conn.execute(command))[0][0])
-        conn.commit()
+        #conn=sql.connect("assign.db")
+        #command = "select count(*) from user;"
+        #n = int(list(conn.execute(command))[0][0])
+        #conn.commit()
         #print(n)
         userDataInJsonFormat = (request.get_json())
-        user_id = userDataInJsonFormat['user_id']
         user_name = userDataInJsonFormat['username']
-        contact_no = userDataInJsonFormat['contact_no']
-        email_id = userDataInJsonFormat['email_id']
         password = userDataInJsonFormat['password']
-        
-        details = [user_id,user_name,contact_no,email_id,password]
+        enc_password = hashlib.sha1(password.encode()) 
+        password = enc_password.hexdigest()
+        details = [user_name,password]
         print(details)
         add_user(details)
         
@@ -42,6 +47,18 @@ def api_add_user():
     return render_template('test.html')
         
 
+@app.route('/api/v1/users/<username>',methods=["POST","GET","DELETE","PUT"])
+def api_delete_user(username):
+    if request.method == 'DELETE':
+        #userDataInJsonFormat = (request.get_json())
+        #print(userDataInJsonFormat)
+        #user_name = userDataInJsonFormat['username']
+        print(username)
+        remove_user(str(username))
+
+        return jsonify({}),200
+
+    return render_template('test.html')
 
 
 
